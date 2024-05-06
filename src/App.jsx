b67ce7fb4d068360c56b619eabeee4af4946c1cb
Sean <fengying1028@hotmail.com>
2024-05-06 11:55:33 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import "./index.css"
-import Tobbar from "./components/Tobbar"
+import "./index.css";
+import Tobbar from "./components/Tobbar";
 // import Login from "@/pages/Login.jsx";
 
 function App() {
-	return (
-		<>
-			<Tobbar></Tobbar>
-		</>
-	);
+  return (
+    <>
+
+
+	
+      <Tobbar></Tobbar>
+    </>
+  );
 }
 
 export default App;
