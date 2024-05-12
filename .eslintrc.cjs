6f6323de7dc3e9ca8b3b7f972b7329831cb30234
Sean <fengying1028@hotmail.com>
2024-05-11 21:39:21 -0600

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    semi:['error', 'always']
+    semi:['error', 'always'],
+    "react/prop-types":"off"
   },
 }
