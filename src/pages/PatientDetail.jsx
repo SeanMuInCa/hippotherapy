ae1702c1827a4ec3b178563838277782090db0b2
Sean <fengying1028@hotmail.com>
2024-05-13 21:09:53 -0600

    test

--- src/pages/PatientDetail.jsx
+++ src/pages/PatientDetail.jsx
@@ -1,12 +1,10 @@
 import { useParams } from "react-router-dom";
 import usePatientStore from "../store/usePatient";
 export default function PatientDetail() {
-    const {id} = useParams();
-    const patientStore = usePatientStore();
-    const data = patientStore[0].data[id];
-    
-    console.log(data);
-  return (
-    <div>PatientDetail</div>
-  );
+  const { id } = useParams();
+  const patientStore = usePatientStore();
+  const data = patientStore[0].data[id];
+
+  console.log(data);
+  return <div>PatientDetail</div>;
 }
