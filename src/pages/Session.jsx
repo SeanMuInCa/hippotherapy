62a91f94d881dbc7d5c88d8061f7c6618f7616c8
Sean <fengying1028@hotmail.com>
2024-05-21 10:46:09 -0600

    test

--- src/pages/Session.jsx
+++ src/pages/Session.jsx
@@ -1,14 +1,16 @@
 import { Button } from "antd";
 import { useParams } from "react-router-dom";
 const Session = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const goAssessment = () => {
     window.location.href = "/assessment/" + id;
   };
   return (
     <>
       <div>Session</div>
-      <Button type="primary" onClick={goAssessment}>new assessment</Button>
+      <Button type="primary" onClick={goAssessment}>
+        new assessment
+      </Button>
     </>
   );
 };
