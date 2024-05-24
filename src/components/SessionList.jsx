ca5e45c21775be2ac1fb7d0b9fc57a38d3929bc8
Sean <fengying1028@hotmail.com>
2024-05-23 21:12:53 -0600

    test

--- src/components/SessionList.jsx
+++ src/components/SessionList.jsx
@@ -2,67 +2,67 @@ import { List } from "antd";
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 const SessionList = () => {
-	const [loading, setLoading] = useState(false);
-	// const [data, setData] = useState([]);
-    const data = [
-        {
-            sessionId:'01',
-            date:'2024-05-22'
-        },
-        {
-            sessionId:'02',
-            date:'2024-05-23'
-        },
-        {
-            sessionId:'03',
-            date:'2024-05-24'
-        },
-        {
-            sessionId:'04',
-            date:'2024-05-25'
-        }
-    ];
-	const loadMoreData = () => {
-		if (loading) {
-			return;
-		}
-		setLoading(true);
-		//get data
-	};
-	useEffect(() => {
-		loadMoreData();
-	}, []);
-	return (
-		<div
-			id="scrollableDiv"
-			style={{
-				height: 300,
-				overflow: "auto",
-				padding: "0 16px",
-				border: "1px solid rgba(140, 140, 140, 0.35)",
-			}}
-		>
-			<InfiniteScroll
-				dataLength={data.length}
-				next={loadMoreData}
-				hasMore={data.length < 50}
-				scrollableTarget="scrollableDiv"
-			>
-				<List
-					dataSource={data}
-					renderItem={(item) => (
-						<List.Item key={item.id}>
-							<List.Item.Meta
-								title={'Session id: '+item.sessionId}
-								description={item.date}
-							/>
-							<div>Detail</div>
-						</List.Item>
-					)}
-				/>
-			</InfiniteScroll>
-		</div>
-	);
+  const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState([]);
+  const data = [
+    {
+      sessionId: "01",
+      date: "2024-05-22",
+    },
+    {
+      sessionId: "02",
+      date: "2024-05-23",
+    },
+    {
+      sessionId: "03",
+      date: "2024-05-24",
+    },
+    {
+      sessionId: "04",
+      date: "2024-05-25",
+    },
+  ];
+  const loadMoreData = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    //get data
+  };
+  useEffect(() => {
+    loadMoreData();
+  }, []);
+  return (
+    <div
+      id="scrollableDiv"
+      style={{
+        height: 300,
+        overflow: "auto",
+        padding: "0 16px",
+        border: "1px solid rgba(140, 140, 140, 0.35)",
+      }}
+    >
+      <InfiniteScroll
+        dataLength={data.length}
+        next={loadMoreData}
+        hasMore={data.length < 50}
+        scrollableTarget="scrollableDiv"
+      >
+        <List
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item key={item.id}>
+              <List.Item.Meta
+                title={"Session id: " + item.sessionId}
+                description={item.date}
+              />
+              <div>Detail</div>
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
+    </div>
+  );
 };
 
 export default SessionList;
